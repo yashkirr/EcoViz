10ba9ee48e6b575d0b95b5795f67dfba556b65b8
Yashkir Ramsamy <me@yashkir.co.za>
2020-08-24 08:40:40 +0200

    Initial commit - very basic UI
    Former-commit-id: 73e2560a9460382c742bc2417ba4a04d82064a7e

---
+++ src/main/java/com/team4/ecoviz/Controller.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team4.ecoviz;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author yashkir
+ */
+public class Controller {
+    
+    public static String loadFile(String name, String fileType){
+        JFileChooser fileDialog = new JFileChooser();
+        fileDialog.setFileFilter(new FileNameExtensionFilter(name, fileType)); 
+        int returnVal = fileDialog.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            return fileDialog.getSelectedFile().getAbsolutePath();
+        }
+        return "No File Selected";
+        }
+    
+}
