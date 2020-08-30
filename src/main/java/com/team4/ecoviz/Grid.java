87c12353c4baee3314b68ca5250362751e27bc01
Victor Bantchovski <victor.bantchovski@gmail.com>
2020-08-30 22:31:13 +0200

    Updated Grid class
    Former-commit-id: 5c8a0af6bb41fae20f9937a5335649b17601d5b6

--- src/main/java/com/team4/ecoviz/Grid.java
+++ src/main/java/com/team4/ecoviz/Grid.java
@@ -8,5 +8,19 @@ public class Grid {
     private static double latitude;
     private static float[][] terrain;
 
+    public Grid(){
+        this.dimx = 0;
+        this.dimy = 0;
+        this.spacing = 0;
+        this.latitude = 0;
+    }
 
+    // Create grid object
+    public Grid(int x, int y, float spacing, float latitude, float[][] terrain){
+        this.dimx = x;
+        this.dimy = y;
+        this.spacing = spacing;
+        this.latitude = latitude;
+        this.terrain = terrain;
+    }
 }
