5c736638239b1aa37e95e0ccd0dae7c5b4c77aec
rmsyas003 <rmsyas003@cs.uct.ac.za>
2020-10-15 03:44:17 +0200

    Label formatting for filters screen

--- src/UserView.java
+++ src/UserView.java
@@ -184,6 +184,20 @@ public class UserView extends JFrame{
             }
         });
 
+        sldMinCanopyRadius.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+
+            }
+        });
+
+        sldMaxCanopyRadius.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+
+            }
+        });
+
         /* Spinners */
         sldRenderingThreshold.addChangeListener(new ChangeListener() {
             @Override
@@ -1396,23 +1410,47 @@ public class UserView extends JFrame{
     private void sldPlantHeightStateChanged(ChangeEvent evt) {
         JSlider source = (JSlider) evt.getSource(); //gets the event type
         pnlVizualizer.heightSliderValue = source.getValue();
+        setSelectedFilterLabel();
         //sliders only work in integers
         if(source.getValue()>=sldPlantHeight.getMaximum()){
              //if slider value minimum, slider label resets to default
         }else{
-            lblPlantHeightValue.setText(Double.toString(source.getValue()));
+            setSelectedFilterLabel();
         }
         pnlVizualizer.repaint();
+    }
+
+    private void setSelectedFilterLabel(){
+        String plantHeightMax = Integer.toString(sldPlantHeight.getValue());
+        String plantHeightMin = Integer.toString(sldPlantHeightMin.getValue());
+        String canopyRadiusMin = Integer.toString(sldMinCanopyRadius.getValue());
+       String canopyRadiusMax = Integer.toString(sldMaxCanopyRadius.getValue());
+       String[] list = {plantHeightMax,plantHeightMin,canopyRadiusMax,canopyRadiusMin};
+        for(int i = 0; i<4;i++){
+            if(list[i].equals("-1")){
+                list[i] = "None";
+            }
+        }
+        String label = "<html>"+"<p><strong>Plant Height: </strong></p>\n" +
+                "<p>Max: %1$s</p>\n" +
+                "<p>Min: %2$s</p>\n" +
+                "<p><b>Range:</b> %1$s - %2$s</p>\n"+
+                "<p><strong>Canopy Radius:</strong>&nbsp;</p>\n" +
+                "<p><b>Max</b>: %3$s</p>\n" +
+                "<p><b>Min</b>: %4$s</p>"+
+                "<p><b>Range:</b> %3$s - %4$s</p>\n";
+        lblPlantHeightValue.setText(String.format(label,plantHeightMin,plantHeightMax,canopyRadiusMin,canopyRadiusMax));
     }
 
     private void sldPlantHeightMinStateChanged(ChangeEvent e) {
         JSlider source = (JSlider) e.getSource(); //gets the event type
         pnlVizualizer.heightMinSliderValue = source.getValue();
+        setSelectedFilterLabel();
         //sliders only work in integers
         if(source.getValue()<=sldPlantHeightMin.getMinimum()){
             //if slider value minimum, slider label resets to default
         }else{
-            lblPlantHeightValue.setText(Double.toString(source.getValue()));
+            setSelectedFilterLabel();
         }
         pnlVizualizer.repaint();
     }
